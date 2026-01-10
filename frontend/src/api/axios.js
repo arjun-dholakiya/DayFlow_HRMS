43657dc1e0219440b47da121b2ddbf0ae1998e7c
Arjun Dholakiya <arjundholakiyav@gmail.com>
2026-01-10 19:49:36 +0530

    fixing some issues

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,19 +1,18 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api/v1'
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
